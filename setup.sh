0325ca5e7e83a6d1be6cbbe571dc8b0e0be2f0cd
Ilan Smith <ilansm@mellanox.com>
2016-11-04 14:08:55 +0200

    Added a help option to setup.sh.
    Usage: ./setup.sh [-h | --help]
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 
 # remember current directory
 CUR_DIR=`pwd`
+DO_HELP=false
 DO_CLEAN=false
+
+COL_BRIGHT="\033[1m"
+COL_NORMAL="\033[0m"
 
 # force script to run from within script directory
 EXEC_PATH=`echo $0 | sed 's/\(.*\)\/[^\/]*$/\1/'`
@@ -34,7 +38,24 @@ exec_script()
 	fi
 }
 
-TEMP=`getopt -o c --long clean -- "$@"`
+help_print_tool()
+{
+	printf "$COL_BRIGHT%-15s$COL_NORMAL - %s.\n" "$1 | $2" "$3"
+}
+
+usage()
+{
+	printf "Usage:\n"
+	printf "     $ $COL_BRIGHT$0 [options]$COL_NORMAL\n"
+	printf "\n"
+	printf "Where 'options' is one of:\n"
+	help_print_tool -c --clean "Clean up (reset) the project environment"
+	help_print_tool -h --help "Display this message and exit"
+	printf "\n"
+	printf "Running without any options sets up the project environment.\n"
+}
+
+TEMP=`getopt -o c,h --long clean,help -- "$@"`
 if [ $? != 0 ]; then
 	echo "Terminating..." >&2;
 	exit 1;
@@ -43,6 +64,10 @@ eval set -- "$TEMP"
 
 while true; do
 	case "$1" in
+		-h|--help)
+			DO_HELP=true
+			shift; break;;
+		--) shift; break;;
 		-c|--clean)
 			DO_CLEAN=true
 			shift; break;;
@@ -50,6 +75,12 @@ while true; do
 	*) echo "Illegal option: $1!"; exit 1;;
 esac
 done
+
+if [ $DO_HELP == "true" ]; then
+	# display usage
+	usage
+	exit 0
+fi
 
 if [ $DO_CLEAN == "true" ]; then
 	# clean the patch directory
