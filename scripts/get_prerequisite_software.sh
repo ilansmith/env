f5e3e2ab47b5642a9498eeb471b8aa068403784b
Ilan Smith <ilansm@mellanox.com>
2017-11-08 10:34:51 +0000

    Support Ubuntu and Red Hat platforms
    Signed-off-by: Ilan Smith <ilansm@mellanox.com>

--- scripts/get_prerequisite_software.sh
+++ scripts/get_prerequisite_software.sh
@@ -10,9 +10,38 @@ source scripts.env
 MISSING=""
 PACKAGES_RECOMMENDED_GENERIC="git valgrind"
 
-test_installed()
+read_os_release()
+{
+	echo "`cat /etc/os-release | grep \"^$1=\" | awk -F= '{ print $2 }' | sed 's%\"%%g'`"
+}
+
+test_installed_ubuntu()
 {
 	dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed"
+}
+
+test_installed_rhel()
+{
+	yum list installed $1 > /dev/null 2>&1; echo $?
+}
+
+test_installed()
+{
+	OS_ID="`read_os_release ID`"
+	OS_NAME="`read_os_release NAME`"
+
+	case $OS_ID in
+		ubuntu)
+			echo `test_installed_ubuntu $1`
+			;;
+		rhel)
+			echo ! `test_installed_rhel $1`
+			;;
+		*)
+			echo "OS $OS_NAME not supported."
+			exit 1
+			;;
+	esac
 }
 
 PRINTLN "Checking for prerequisite software..."
