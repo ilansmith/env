21b8868ad665a52c72ebf6baa29a8293f47a550b
Ilan Smith <ilansm@nvidia.com>
2021-09-11 16:47:50 +0300

    Modify for Linux image pre-prerequisites
    Branched out of: android_kk_octa5422_samsung
    
    Signed-off-by: Ilan Smith <ilansm@nvidia.com>

--- scripts/get_prerequisite_software.sh
+++ scripts/get_prerequisite_software.sh
@@ -39,27 +39,6 @@ setup_executable()
   sudo chmod a+x $EXECUTABLE_PATH/$1
 }
 
-setup_adb()
-{
-  setup_executable adb http://sys-mobilehost/tools/adb/adb.tar.bz2
-}
-
-setup_fastboot()
-{
-  setup_executable fastboot http://sys-mobilehost/tools/fastboot/fastboot.tar.bz2
-}
-
-setup_mkimage()
-{
-  if [ -n "`uname -a | grep x86_64`" ]; then
-	  MKIMAGE_ARCH=64
-  else
-	  MKIMAGE_ARCH=32
-  fi
-
-  setup_executable mkimage http://sys-mobilehost/tools/mkimage/mkimage_$MKIMAGE_ARCH.tar.bz2
-}
-
 setup_ccache()
 {
   PRINTLN "Limiting ccache size..."
@@ -70,8 +49,5 @@ PRINTLN "Installing prerequisite software..."
 apt_get_update
 install_common_recommended
 [ -n "`uname -a | grep x86_64`" ] && install_64bit_recommended
-[ -z "`which adb`" ] && setup_adb
-[ -z "`which fastboot`" ] && setup_fastboot
-[ -z "`which mkimage`" ] && setup_mkimage
 [ -n "`which ccache`" ] && setup_ccache
 
